09853c2405e84aa71a1b7eb2c3b15216d2fb9716
akshatgg <akshatg9636@gmail.com>
2023-08-20 16:55:10 +0545

    research

---
+++ research.js
@@ -0,0 +1,10 @@
+const PWCoursePrice =200
+const GST=20
+let TotalPrice = PWCoursePrice + GST
+console.log("Final amoun to be paid = ",TotalPrice);
+const DSACoursePrice=300
+console.log("Final amoun to be paid = ",TotalPrice+DSACoursePrice);
+
+console.log("Answer is ",5/2);
+console.log("Answer is ",5%2);
+console.log("Answer is ",5**2);   //to the power//
