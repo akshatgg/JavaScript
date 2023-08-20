850ea343048cb57e22161f4110412fe8937d712f
akshatgg <akshatg9636@gmail.com>
2023-08-21 00:38:13 +0545

    do while

---
+++ Loops.js
@@ -0,0 +1,26 @@
+//for loop//
+
+for(let i=1;i<=6;i++){
+    console.log(i);
+}
+
+
+//while loop//
+let j=0;
+while(j<5){
+    console.log(j);
+    j++;
+}
+ 
+
+
+//do while loop//
+
+let k=0;
+do{
+    console.log("Hello world");
+    k++;
+    
+}while(k<5);
+
+//
