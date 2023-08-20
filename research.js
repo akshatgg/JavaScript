158cbd8cc1ac39518a8ac50ab962b6d93715ec40
akshatgg <akshatg9636@gmail.com>
2023-08-20 17:07:21 +0545

    practice

--- research.js
+++ research.js
@@ -8,3 +8,21 @@ console.log("Final amoun to be paid = ",TotalPrice+DSACoursePrice);
 console.log("Answer is ",5/2);
 console.log("Answer is ",5%2);
 console.log("Answer is ",5**2);   //to the power//
+
+//comparison//
+const rajHighScore=200
+const tejHighScore=200
+const SohamHighScore="200"
+console.log(rajHighScore==tejHighScore);
+console.log(SohamHighScore==rajHighScore);
+console.log(SohamHighScore===tejHighScore);
+console.log(SohamHighScore!==tejHighScore);
+
+let gmailaccount=true
+let emailaccount=false
+console.log(gmailaccount || emailaccount);
+
+
+
+
+
