32be2cd2f674d7ec990a307821314ee0ad3ffb9e
akshatgg <akshatg9636@gmail.com>
2023-08-21 00:30:19 +0545

    Condition

---
+++ ConditionAndLoops.js
@@ -0,0 +1,26 @@
+let light="red";
+if(light=="green"){
+    console.log("Go")
+}
+else if(light=="yellow"){
+    console.log("Ready to start");
+}
+else if(light=="red"){
+    console.log("Stop");
+}
+else{
+    console.log("Invalid");
+}
+
+
+//Switch//
+
+let user="admin"
+switch(user){
+    case "admin":
+        console.log("He is a admin");
+        break;
+        case "Strudent":
+            console.log("He is a Student")
+            break;
+}
