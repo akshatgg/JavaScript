6e9091d248aa3b8ce21274200f24e4917f71cb4f
akshatgg <akshatg9636@gmail.com>
2023-08-21 13:19:17 +0545

    research

---
+++ practice.js
@@ -0,0 +1,80 @@
+var num=8
+var num=10
+console.log(num);
+ 
+
+
+
+console.log(typeof typeof 1);
+
+
+
+
+let a=3;
+let b=new Number(3)
+let c=3;
+console.log(a==b);
+console.log(a===b);
+console.log(b===c);
+
+
+let greeting;
+greetign={};
+console.log(greetign);
+
+
+
+let number=0
+console.log(number++);
+console.log(++number);
+console.log(number);
+
+
+
+const numbers=[1,2,3];
+numbers[10]=11
+console.log(numbers);
+
+
+console.log(3+ 4+"5");
+//output is 75//
+
+
+console.log("I enrolled to the PW FWSD course"[2]);
+//outout is e//
+
+
+
+// const name=Skills
+// console.log(name());
+//output is undefined//
+
+
+
+
+const one=false || {} || null
+const two=false || null || ""
+const three=[] || true || 0
+console.log(one,two,three);
+//priority is [],{},"",null
+
+
+var x=2;
+var y="2"
+console.log(x===y);
+
+
+for(let i=1;i<5;i++){
+    if(i==3)continue;
+    console.log(i);
+}
+
+
+let k=5/0
+console.log(k);
+
+
+
+const tim="Skills"
+console.log(!typeof tim ==="object");
+console.log(!typeof tim==="string");
