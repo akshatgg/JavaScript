00bfd1953a6bbc4d90fcff5bc436c2dadafb5b79
akshatgg <akshatg9636@gmail.com>
2023-08-21 14:42:19 +0545

    modify

---
+++ Array.js
@@ -0,0 +1,27 @@
+
+//Define Array//
+
+let a=[2,4,"string","Tom"];
+console.log(a);
+
+
+let b=new Array(5)
+console.log(b); 
+
+ //add element in  array//
+console.log(a[3]);
+a[3]=5
+console.log(a);
+
+
+let e=[4,3,2]
+e.push(3,9)
+console.log(e);
+e.pop()        //delete last element of an array//
+console.log(e);
+e.shift()     // delete 1 item rom the starting of an array//
+console.log(e);
+e.push(5,9,10)
+console.log(e);
+e.unshift(9)        //shift that number in the irst postion//
+console.log(e);
