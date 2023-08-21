4a18037fb584e960a3e4723ef0c966afbbef082a
akshatgg <akshatg9636@gmail.com>
2023-08-21 17:52:57 +0545

    modify

--- Function.js
+++ Function.js
@@ -8,3 +8,25 @@ function sqr(x){
 }
 let a=sqr(5)
 console.log(a);
+
+
+
+function add(x,y){
+    return x+y;
+}
+console.log(add(5,9))
+
+
+    function sumofallparameter(){
+        let sum=0;
+        for(var i=0;i<arguments.length;i++){
+            sum=sum+arguments[i];
+        }
+        return sum;
+    }
+
+    let result=sumofallparameter(1,2,3,4,5);
+    console.log(result);
+
+
+ 
