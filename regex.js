be4b07cca379931a905044ed8730a4eee7f78d48
akshatgg <akshatg9636@gmail.com>
2023-08-25 20:20:11 +0545

    regex

--- regex.js
+++ regex.js
@@ -1,13 +1,18 @@
 let pattern='pw';
 
-let regexOne=new RegExp(pattern);
+let regexOne=new RegExp(pattern);          //default flag is g//
 
 let flag='gi';
 let regexTwo=new RegExp(pattern,flag);
 
 let regexThree=/pw/gi;
 
-const content='My name is Akshat Gupta.I am studying regex from pw';
+const content='My name is Akshat Gupta.I am studying regex from Pw.pw is good in some ways.your skills is not depend on your source its depends on your practice thanks pW';
 
 let check=regexOne.test(content);
 console.log(check);
+
+let check1=content.match(regexThree);
+console.log(check1);
+
+
