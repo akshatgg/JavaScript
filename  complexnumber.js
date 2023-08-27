387de032f8f247db6c671da12c40d39d88afaef7
akshatgg <akshatg9636@gmail.com>
2023-08-28 02:00:58 +0545

    sub

---  complexnumber.js
+++  complexnumber.js
@@ -11,17 +11,32 @@ this.#real=r;
     display(){
         console.log(this.#real,"+i",this.#img);
     }
-   get imag(){
+   get img(){
     return this.#img;
    }
    get real(){
     return this.#real;
    }
     addcomplexnumb(c1){
-            this.#img=this.img+c1.imag;
+            this.#img=this.img+c1.img;
             this.#real=this.#real+c1.real 
     }
 }
 const p=new complexnumber(3,4);
 // console.log(p);
-p.display()
+let c1=new complexnumber(5,8)
+c1.addcomplexnumb(p);
+c1.display()
+
+class sub{
+    constructor(r,i){
+   this.real=r;
+   this.img=i;
+    }
+    subs(c2){
+        console.log(this.real-c2.real,"i",this.img-c2.img);
+    }
+}
+let co1=new sub(9,9);
+let c2=new sub(6,6);
+co1.subs(c2);
