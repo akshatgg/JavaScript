2bbb79e7bb91f6c71ef44f31854aef3bf7320c43
akshatgg <akshatg9636@gmail.com>
2023-08-27 14:27:05 +0545

    this class

--- tryCatchClass.js
+++ tryCatchClass.js
@@ -6,3 +6,24 @@ try{
 }finally{
     console.log("finally");
 }
+
+//Classes//
+class practice{
+    name;
+    salary;
+    Number;
+    constructor(n,s,N){
+       console.log("value");
+        this.name=n;
+        this.salary=s;
+        this.Number=N;
+    }
+    display(){
+        console.log("thank yu");
+        console.log("this refers to",this);
+    }
+}
+const p=new practice("akshat","2000","89")                    //  new create a empty object always  // 
+console.log(p);
+p.display()
+
