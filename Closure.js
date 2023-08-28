ab41c12405eeb965e4befba0ee2cb14c2e340b67
akshatgg <akshatg9636@gmail.com>
2023-08-28 22:07:07 +0545

    closure

--- Closure.js
+++ Closure.js
@@ -1,6 +1,11 @@
 let fun1=()=>{
     const value1="I am in fun right now";
     const innerfun1=()=>{
-        const value2="I am not in fun right now"
+        const value2="I am not in fun right now";
+        console.log(value1);
     }
+    innerfun1();
+    // console.log(value2);              //error comes//
 }
+fun1();
+
