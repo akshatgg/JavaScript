f9ee4e815aad2ec16ae62944684a9bca5482cb77
akshatgg <akshatg9636@gmail.com>
2023-08-28 17:31:32 +0545

    modify

--- prototype.js
+++ prototype.js
@@ -15,3 +15,45 @@ Object.prototype.hitesh=()=>{
 }
 console.log(myhero.hitesh());
 console.log(heropower.spidypower());
+console.log(heropower.hitesh());
+console.log(dchero.hitesh());
+
+Array.prototype.heyhitesh=function(){
+    console.log("hi hitesh");
+}
+console.log(myhero.heyhitesh());
+// console.log(heropower.heyhitesh());       //comes error because this prototype is only allowed in array//
+
+
+//inheritance//
+
+
+const user={
+    name:"top name",
+    email:"top@gmail.com"
+}
+const teacher={
+    makevideos:true,
+}
+const teachersupport={
+    isavailaible:false,
+}
+const taassistant={
+    makeassignment:"js",
+    fulltime:true,
+    __proto__:teachersupport,               //inheritance M1//
+}
+teacher.__proto__=teachersupport;           //inheritance M1//
+console.log(teacher.isavailaible);
+console.log(taassistant.isavailaible);     
+Object.setPrototypeOf(user,teacher);         //inheritance M2//
+console.log(user.makevideos);
+
+
+
+String.prototype.lengthx=function(nam){
+    console.log('True length is ',this.trim().length);
+}
+// "hitesh..      ".lengthx();
+const nam="akshat   ";
+nam.lengthx();
